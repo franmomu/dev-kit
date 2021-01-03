e3f115dbf500662c04ef3aa7c2df80917cecf3c1
SonataCI <thomas+ci@sonata-project.org>
2021-01-03 11:45:00 +0000

    Fix: Run 'php-cs-fixer fix'

--- src/Domain/Value/Project.php
+++ src/Domain/Value/Project.php
@@ -77,7 +77,6 @@ final class Project
 
     public static function fromValues(string $name, array $config, Package $package): self
     {
-        
         $branches = [];
         foreach ($config['branches'] as $branchName => $branchConfig) {
             $branches[] = Branch::fromValues($branchName, $branchConfig);
